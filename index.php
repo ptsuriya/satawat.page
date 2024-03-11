067a6d201ffb8d82bfd276430167d5422858866e
PORAMART TSURIYARUNGSRI <pongthebigbear@gmail.com>
2024-03-11 21:10:43 +0700

    update

---
+++ index.php
@@ -0,0 +1,172 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+  <title>Satawat Page</title>
+
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=Edge">
+  <meta name="description" content="">
+  <meta name="keywords" content="">
+  <meta name="author" content="Tooplate">
+  <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+
+  <link rel="stylesheet" href="css/bootstrap.min.css">
+  <link rel="stylesheet" href="css/all.min.css">
+  <link rel="stylesheet" href="css/owl.carousel.min.css">
+  <link rel="stylesheet" href="css/owl.theme.default.min.css">
+
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Kanit:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
+  <!-- MAIN CSS -->
+  <link rel="stylesheet" href="css/tooplate-ben-resume-style.css">
+  <!--
+Tooplate 2120 Ben Resume
+https://www.tooplate.com/view/2120-ben-resume
+-->
+</head>
+
+<body data-spy="scroll" data-target="#navbarNav" data-offset="50">
+
+  <!-- MENU BAR -->
+  <nav class="navbar navbar-expand-lg">
+    <div class="container">
+
+      <a class="navbar-brand" href="#">
+        Satawat
+      </a>
+
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav mx-auto">
+          <li class="nav-item">
+            <a href="#intro" class="nav-link smoothScroll">Introduction</a>
+          </li>
+
+          <li class="nav-item">
+            <a href="#about" class="nav-link smoothScroll">About Me</a>
+          </li>
+
+          <li class="nav-item">
+            <a href="#research" class="nav-link smoothScroll">research</a>
+          </li>
+        </ul>
+
+        <div class="mt-lg-0 mt-3 mb-4 mb-lg-0">
+          <a href="#slide" class="custom-btn btn">ดูสไลด์เรียนทั้งหมด</a>
+        </div>
+      </div>
+
+    </div>
+  </nav>
+
+
+  <!-- HERO -->
+  <section class="hero d-flex flex-column justify-content-center align-items-center" id="intro">
+
+    <div class="container">
+      <div class="row">
+
+        <div class="mx-auto col-lg-5 col-md-5 col-10">
+          <img src="images/satawat.png" class="img-fluid" alt="Satawat HTML Template">
+        </div>
+
+        <div class="d-flex flex-column justify-content-center align-items-center col-lg-7 col-md-7 col-12">
+          <div class="hero-text">
+
+            <h1 class="hero-title">Asst.prof.Satawat ThipSod</h1>
+
+            <a href="#" class="email-link">
+              Satawat.t@rbru.ac.th
+            </a>
+
+          </div>
+        </div>
+
+      </div>
+    </div>
+  </section>
+
+
+  <section class="about section-padding" id="about">
+    <div class="container">
+      <div class="row">
+
+        <div class="col-lg-6 col-md-6 col-12">
+          <h3 class="mb-4">ข้อมูลส่วนตัว</h3>
+          <ul class="mt-4 mb-5 mb-lg-0 profile-list list-unstyled">
+            <li><strong>ผู้ช่วยศาสตราจารย์</strong> ศตวรรษ ทิพโสต </li>
+            <li><strong>Assisstant Professor</strong> Satawat Thipsod </li>
+            <li>
+            Marketing, Faculty of Management Science
+            Rambhai Barni Rajabhat University</li>
+          </ul>
+          
+          <h5 class="mt-5">การศึกษา</h5>
+          <h6>ปริญญาโท</h6>
+          <p><b>บธ.ม.(การตลาด) มหาวิทยาลัยเทคโนโลยีมหานคร</b></p>
+          <h6>ปริญญาตรี</h6>
+          <p><b> บธ.บ.(การตลาด) มหาวิทยาลัยรามคำแหง</b></p>
+
+
+        </div>
+
+        <div class="col-lg-5 mx-auto col-md-6 col-12">
+          <img src="images/333619.jpg" class="about-image img-fluid" alt="Ben's Resume HTML Template">
+        </div>
+
+      </div>
+      <div class="row about-third" id="research">
+        <div class="col-lg-4 col-md-4 col-12">
+          <h6>ผลงานวิจัย</h6>
+         <ul class="mt-4 mb-5 mb-lg-0 profile-list list-unstyled">
+          <li>1</li>
+          <li>2</li>
+          <li>3</li>
+          <li>4</li>
+         </ul>
+        </div>
+        <div class="col-lg-4 col-md-4 col-12">
+          <h6>ผลงานวิชาการ</h6>
+          <ul class="mt-4 mb-5 mb-lg-0 profile-list list-unstyled">
+          <li>1</li>
+          <li>2</li>
+          <li>3</li>
+          <li>4</li>
+         </ul>
+        </div>
+        <div class="col-lg-4 col-md-4 col-12">
+          <h6>ความเชี่ยวชาญและความสนใจ</h6>
+          <ul class="mt-4 mb-5 mb-lg-0 profile-list list-unstyled">
+          <li>1</li>
+          <li>2</li>
+          <li>3</li>
+          <li>4</li>
+         </ul>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <section id="slide">
+    <div class="container">
+    <h3 class="mb-4">เอกสารประกอบการสอน</h3>
+  <iframe src="https://drive.google.com/embeddedfolderview?id=1phYluMqYWaA5YcUiEtdbXVy3Jg7FqV85#list" style="width:100%; height:600px; border:0;"></iframe>
+    </div>
+  </section>
+  
+  <!-- SCRIPTS -->
+  <script src="js/jquery.min.js"></script>
+  <script src="js/bootstrap.min.js"></script>
+  <script src="js/smoothscroll.js"></script>
+  <script src="js/owl.carousel.min.js"></script>
+  <script src="js/custom.js"></script>
+
+</body>
+
+</html>
